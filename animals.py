86f76f56854f5f5c6d3a1a2eabdaf0a106f5d481
Baes14 <badou.ba@me.com>
2018-01-02 21:56:36 +0100

    .

---
+++ animals.py
@@ -0,0 +1,4 @@
+animals=["Lion", "Tiger", "Panther"]
+for animal in animals:
+	print("A "+animal+" is a dangerous wild animal")
+print("\nAll these animals live in the wilderness")
---
+++ pizzas.py
@@ -0,0 +1,4 @@
+pizzas=["4 season", "Spicy hot one", "Meat lovers", "Mexican", "Seafood"]
+for pizza in pizzas:
+	print("I love the "+pizza+" pizza !")
+print("\nI'm a huge pizza fan!")
